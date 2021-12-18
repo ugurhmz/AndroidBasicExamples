6506ae7da00385df97e6a550d7fb099cec86fdd8
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-19 01:16:54 +0300

    AlertDialog & Toast message, yes-no pop-up

--- AlertDialog/app/src/main/java/com/ugurhmz/alertdialog/MainActivity.java
+++ AlertDialog/app/src/main/java/com/ugurhmz/alertdialog/MainActivity.java
@@ -1,14 +1,50 @@
 package com.ugurhmz.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    // Save butonuna bastıktan sonra -> Alert ver.
+    public void save(View view) {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+
+        // Positive pop-up
+        alert.setPositiveButton("Yes",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                Toast.makeText(MainActivity.this,"Saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        // Negative pop-up
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Not Saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
     }
 }
