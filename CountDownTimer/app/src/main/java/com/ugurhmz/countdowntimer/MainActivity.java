2888583a1a9c5c8d7a216de46c3e98f0ff4db846
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-19 17:47:22 +0300

    CountDownTimer

--- CountDownTimer/app/src/main/java/com/ugurhmz/countdowntimer/MainActivity.java
+++ CountDownTimer/app/src/main/java/com/ugurhmz/countdowntimer/MainActivity.java
@@ -3,12 +3,33 @@ package com.ugurhmz.countdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        new CountDownTimer(10000,1000) {
+
+            @Override
+            public void onTick(long l) {
+                    textView.setText("left : " + l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
+            }
+        }.start();
     }
 }
